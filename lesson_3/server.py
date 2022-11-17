0a9aaa233a1fc0ee493353de661772fce1eee8c1
IUseAll16Letters <ch1ck3n@mail.ru>
2022-11-17 07:27:12 +0300

    minor changes

--- lesson_3/server.py
+++ lesson_3/server.py
@@ -2,7 +2,7 @@ import asyncio
 
 from asyncio.streams import StreamWriter, StreamReader
 from utils.config import *
-from utils.utils import make_message, handle_message, is_authorized
+from utils.utils import make_message, handle_message, is_authorized, authorize
 
 
 async def handler(reader: StreamReader, writer: StreamWriter):
@@ -10,20 +10,17 @@ async def handler(reader: StreamReader, writer: StreamWriter):
     print(f'User {writer.get_extra_info("peername")} connected | init message ...')
     user_data = handle_message(await reader.read(PACKAGE_SIZE), writer)
     print(f'{user_data = }')
-    try:
-        username = user_data['user']['username']
-        # always authorized
-        if is_authorized(username, authorized):
-            print('User authorized')
-            writer.write(make_message(Message.probe))
-            await writer.drain()
 
-            user_response_2 = handle_message(await reader.read(PACKAGE_SIZE), writer)
+    # always authorized
+    if is_authorized(user_data, authorized) or authorize(user_data):
+        print('User authorized')
+        writer.write(make_message(Message.probe))
+        await writer.drain()
 
-    except KeyError:
-        print('no username passed, fault message disconnecting user')
-        writer.close()
-        return -1
+        user_response_2 = handle_message(await reader.read(PACKAGE_SIZE), writer)
+    else:
+        print('Is not authorized')
+
 
     return -1
 
--- lesson_3/utils/utils.py
+++ lesson_3/utils/utils.py
@@ -1,7 +1,12 @@
 import json
 import datetime
 
+from hashlib import sha256
 from .config import *
+
+
+class AuthorizationError(ConnectionError):
+    pass
 
 
 def make_message(message_type: Message, **kwargs):
@@ -34,6 +39,37 @@ def handle_message(message: bytes, writer):
 
 def initialize():
     pass
+
+
+def make_secure_password(password: str) -> str:
+    return sha256((PASSWORD_SALT + password).encode(ENCODING)).hexdigest().upper()
+
+
+def verify_password(username: str, password: str, db_object: dict) -> bool:
+    password_hash = make_secure_password(password)
+    user_password = db_object.get(username).get('password')
+    return user_password == password_hash
+
+
+def get_user_data_from_set(user_dataset: dict):
+    try:
+        return user_dataset['user']['username']
+    except KeyError:
+        print("Wrong data set passed")
+        return False
+
+
+def authorize(user_data, db_object):
+    try:
+        username, password = get_user_data_from_set(user_data), 'password'
+        if verify_password(username, password, db_object):
+            return True
+        else:
+            raise AuthorizationError
+
+    except AuthorizationError:
+        print("Wrong account, password data")
+    return False
 
 
 def is_authorized(username, authorized):
